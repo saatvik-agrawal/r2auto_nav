4347708560ac41e06ba44c0beabeed28bfe7f4f4
jglee11 <93311706+jglee11@users.noreply.github.com>
2023-04-04 10:49:35 +0800

    Update publisher.py

--- planA/publisher.py
+++ planA/publisher.py
@@ -3,10 +3,13 @@ import ubinascii
 import machine
 from umqtt.simple import MQTTClient
 
-MQTT_BROKER = '192.168.1.124'
+MQTT_BROKER = '192.168.128.219'
+#remember to change this to your laptop IP Address
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 user = "roger"
+#remember to change this to user in your passwordfile
 password = "password"
+#remember to change this to password in your passwordfile
 MQTT_TOPIC = "client"
 
 def reset():
