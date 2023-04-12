abcedba0a1277ade05f6ffea1daa80b545bc6be2
jglee11 <93311706+jglee11@users.noreply.github.com>
2023-04-12 21:13:33 +0800

    Update IRLineNav.py

--- planA/IRLineNav.py
+++ planA/IRLineNav.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int16
 import time
 
 L=3
@@ -19,14 +19,10 @@ speedchange = -0.02
 class Docker(Node):
   def __init__(self):
     super().__init__('docking')
-    self.mover = self.create_publisher(Twist,'cmd_vel',10)
-    #self.canstate = self.create_publisher(Bool, 'can', 10)
-    self.following = True
-    self.onoroff = self.create_subscription(Bool,'docking_status', self.followersub, 5)
+    self.canstate = self.create_publisher(Bool, 'can', 10)
+    self.leftPub = self.create_publisher(Int16, IRLeft, 3)
+    self.rightPub = self.create_publisher(Int16, IRRight, 3)
   
-  def followersub(self, msg):
-    self.following = True #msg.data
-
   def canstatus(self):
     msg = Bool()
     if GPIO.input(cansensor) == 1:
@@ -35,75 +31,22 @@ class Docker(Node):
       msg.data = False
     self.canstate.publish(msg)
 
-  def stopbot(self):
-    twist = Twist()
-    twist.linear.x = 0.0
-    twist.angular.z = 0.0
-    self.mover.publish(twist)
+  def IRPublish(self):
+    leftmsg = Int16()
+    rightmsg = Int16()
+    leftmsg = GPIO.input(L)
+    rightmsg = GPIO.input(R)
+    self.leftPub.publish(leftmsg)
+    self.rightPub.publish(rightmsg)
+    
 
-  def IR_follow(self):
-    twist = Twist()
-    print("in IR_follow")
-    follow = True
-    extreme = True
-    while (follow == True):
-      print(GPIO.input(L))
-      print(GPIO.input(R))
-      if(GPIO.input(L)==0 and GPIO.input(R)==0): #Front
-        twist.linear.x = speedchange
-        twist.angular.z = 0.0
-        print(twist.linear.x)
-        time.sleep(1)
-        self.mover.publish(twist)
-        print("publishing")
-        if (extreme == True and GPIO.input(L)!=0 and GPIO.input(R)!=0): #extreme case where bot is perpendicular, run once
-          twist.linear.x = 0.0
-          twist.angular.z = 0.0
-          time.sleep(1)
-          self.mover.publish(twist)
-          while (GPIO.input(L)!=0 and GPIO.input(R)!=0):
-            twist.linear.x = -0.02
-            time.sleep(0.1)
-            self.mover.publish(twist)
-            twist.linear.x = 0.0
-            twist.angular.z = 0.1
-            time.sleep(0.1)
-            self.mover.publish(twist)
-            time.sleep(1)
-            twist.angular.z = 0.0
-            time.sleep(0.1)
-            self.mover.publish(twist)
-          extreme = False
-      elif (GPIO.input(L)==0 and GPIO.input(R)!=0): #Right (Clockwise)
-        twist.linear.x = 0.0
-        twist.angular.z = -rotatechange
-        self.mover.publish(twist)
-        extreme = False
-      elif (GPIO.input(L)!=0 and GPIO.input(R)==0): #Left (Counter-Clockwise)
-        twist.linear.x = 0.0
-        twist.angular.z = rotatechange
-        time.sleep(0.1)
-        self.mover.publish(twist)
-        extreme = False
-      else: #Dont move
-        twist.linear.x = 0.0
-        twist.angular.z = 0.0
-        self.mover.publish(twist)
-        follow = False
-
-  def docking(self):
+  def publisher(self):
     try:
       print("running")
       while True:
-        print("following")
-        self.IR_follow() #to be removed
-        #rclpy.spin_once(self)
-        #print("spin done")
-        #if self.following == True:
-        #  print("IR Following/Docking")
-        #  self.IR_follow()
-        #self.canstate.publish()
-        #time.sleep(1)
+        print("publishing")
+        self.canstatus()
+        self.IRPublish()
 
     except Exception as e:
       print(e)
@@ -116,7 +59,7 @@ def main(args = None):
   print("program start")
   docker = Docker()
   print("docker assigned")
-  docker.docking()
+  docker.publisher()
 
   docker.destroy_node()
   rclpy.shutdown()
